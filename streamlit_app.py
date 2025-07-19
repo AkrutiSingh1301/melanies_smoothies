c5698ac3e5842f4b8231909f35f46724901e0a3e
AkrutiSingh1301 <136474368+AkrutiSingh1301@users.noreply.github.com>
2025-07-19 17:03:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,5 @@ if ingredients_list:
 
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit")
 st.text(smoothiefroot_response)
